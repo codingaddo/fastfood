2a1a114b9eff47db896982604b248ea009a9d290
Addo Michael <119521479+codingaddo@users.noreply.github.com>
2024-04-23 23:06:18 +0000

    Update Whatsapp.jsx

--- app/components/Whatsapp.jsx
+++ app/components/Whatsapp.jsx
@@ -5,7 +5,7 @@ import { FaWhatsapp } from 'react-icons/fa'
 const Whatsapp = () => {
   return (
     <>
-    <Link href={'https://wa.me/+15197254996'} className= ' animate-pulse fixed  p-2 right-0 md:right-0 md:bottom-44 bottom-64 rounded-l-lg z-30 cursor-pointer  bg-[#fa6969] text-center hover:scale-[1.1]'>
+    <Link href={'https://wa.me/+15197254996'} target='_blank' className= ' animate-pulse fixed  p-2 right-0 md:right-0 md:bottom-44 bottom-64 rounded-l-lg z-30 cursor-pointer  bg-[#fa6969] text-center hover:scale-[1.1]'>
         <FaWhatsapp className='text-white text-5xl'/>
     </Link>
     </>
