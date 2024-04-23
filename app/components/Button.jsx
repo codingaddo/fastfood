34ee3ccd3bbf055994b374fd7392771e700ae19b
Addo Michael <119521479+codingaddo@users.noreply.github.com>
2024-04-23 23:36:02 +0000

    .

--- app/components/Button.jsx
+++ app/components/Button.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 const Button = ({txt,url,icon,width}) => {
   let w = '150px'
   return (
-    <Link href={`${url}`} className={`bg-[#fa6969] text-white flex items-center gap-3 justify-center text-center text-lg md:text-sm rounded-2xl  md:font-semibold md:py-4 py-2 px-5 w-[${width || w}] ease-in-out duration-500 shadow-xl hover:shadow-2xl hover:scale-[1.05] hover:bg-white hover:text-[#fa6969]`}>
+    <Link href={`${url}`} className={`bg-[#fa6969] text-white flex items-center gap-3 justify-center text-center text-lg md:text-sm rounded-2xl  md:font-semibold md:py-4 py-2 px-5 w-[150px] ease-in-out duration-500 shadow-xl hover:shadow-2xl hover:scale-[1.05] hover:bg-white hover:text-[#fa6969]`}>
         {icon}
         {txt}
       
