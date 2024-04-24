bfc880299408f9c8e184b3421a63cee8bf0aab2e
Addo Michael <119521479+codingaddo@users.noreply.github.com>
2024-04-24 00:40:13 +0000

    .

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -31,11 +31,11 @@ const Navbar = ()=>{
                 <h className='text-xl text-black font-bold'>Habibis</h>
             </div>
             <ul className="hidden md:flex justify-between items-center gap-10 font-medium">
-                <li className="hover:text-red-600"><Link className={pathName==='/' ?"text-[#fa6969]" :''} href={'/'}>Home</Link></li>
-                <li className="hover:text-red-600"><Link className={pathName==='/about' ?"text-[#fa6969]" :''} href={'/about'}>About</Link></li>
-                <li className="hover:text-red-600"><Link className={pathName==='/menu' ?"text-[#fa6969]" :''} href={'/menu'}>Menu</Link></li>
-                <li className="hover:text-red-600"><Link className={pathName==='/catering' ?"text-[#fa6969]" :''} href={'/catering'}>Catering</Link></li>
-                <li className="hover:text-red-600"><Link className={pathName==='/contact' ?"text-[#fa6969]" :''} href={'/contact'}>Contact</Link></li>
+                <li className="hover:text-red-600 uppercase"><Link className={pathName==='/' ?"text-[#fa6969]" :''} href={'/'}>Home</Link></li>
+                <li className="hover:text-red-600 uppercase"><Link className={pathName==='/about' ?"text-[#fa6969]" :''} href={'/about'}>About</Link></li>
+                <li className="hover:text-red-600 uppercase"><Link className={pathName==='/menu' ?"text-[#fa6969]" :''} href={'/menu'}>Menu</Link></li>
+                <li className="hover:text-red-600 uppercase"><Link className={pathName==='/catering' ?"text-[#fa6969]" :''} href={'/catering'}>Catering</Link></li>
+                <li className="hover:text-red-600 uppercase"><Link className={pathName==='/contact' ?"text-[#fa6969]" :''} href={'/contact'}>Contact</Link></li>
             </ul>
         <div className={isOpen? " md:hidden pb-4 menu open cursor-pointer " :"md:hidden pb-4 menu cursor-pointer "} onClick={handleOpenNav}>
             <div className="dash top "></div>
@@ -49,10 +49,10 @@ const Navbar = ()=>{
         <div className={isOpen?" open-nav  z-50 p-10 md:hidden":"Rnav  z-50 p-10 md:hidden"} onClick={()=>{setIsOpen(false)}}>
              <ul className="text-xl flex flex-col gap-4 bg-slate-200 relative left-28 bottom-10 p-10 pb-20 rounded-bl-xl shadow-2xl">
             <li className="hover:text-red-600"><Link  href={'/'} className={pathName==='/' ?"text-[#fa6969]" :''} onClick={()=>{setIsOpen(false)}}>Home</Link></li>
-                <li className="hover:text-red-600"><Link className={pathName==='/about' ?"text-[#fa6969]" :''} href={'/about'} onClick={()=>{setIsOpen(false)}}>About</Link></li>
-                <li className="hover:text-red-600"><Link className={pathName==='/menu' ?"text-[#fa6969]" :''} href={'/menu'} onClick={()=>{setIsOpen(false)}}>Menu</Link></li>
-                <li className="hover:text-red-600"><Link className={pathName==='/catering' ?"text-[#fa6969]" :''} href={'/catering'} onClick={()=>{setIsOpen(false)}}>Catering</Link></li>
-                <li className="hover:text-red-600"><Link className={pathName==='/contact' ?"text-[#fa6969]" :''} href={'/contact'} onClick={()=>{setIsOpen(false)}}>Contact</Link></li>
+                <li className="hover:text-red-600 uppercase"><Link className={pathName==='/about' ?"text-[#fa6969]" :''} href={'/about'} onClick={()=>{setIsOpen(false)}}>About</Link></li>
+                <li className="hover:text-red-600 uppercase"><Link className={pathName==='/menu' ?"text-[#fa6969]" :''} href={'/menu'} onClick={()=>{setIsOpen(false)}}>Menu</Link></li>
+                <li className="hover:text-red-600 uppercase"><Link className={pathName==='/catering' ?"text-[#fa6969]" :''} href={'/catering'} onClick={()=>{setIsOpen(false)}}>Catering</Link></li>
+                <li className="hover:text-red-600 uppercase"><Link className={pathName==='/contact' ?"text-[#fa6969]" :''} href={'/contact'} onClick={()=>{setIsOpen(false)}}>Contact</Link></li>
             </ul> 
         </div>
     </>
--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,5 @@
 
-import { Inter } from "next/font/google";
+import { Inter, Roboto_Condensed, Roboto_Flex, Roboto_Serif, Roboto_Slab  } from "next/font/google";
 import "./globals.css";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
@@ -7,7 +7,8 @@ import Whatsapp from "./components/Whatsapp";
 import BackToTop from "./components/BackToTop";
 import Provider from "./context/Provider";
 
-// const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ["latin"], display:'swap' });
+const roboto = Roboto_Condensed({ subsets: ["latin"], display:'swap',weight:'500' });
 
 export const metadata = {
   title: "Habibis|Home",
@@ -17,7 +18,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
 
   return (
-    <html lang="en">
+    <html lang="en" className={roboto.className}>
       <body className={''}>
       <Provider>
        <Navbar/>
